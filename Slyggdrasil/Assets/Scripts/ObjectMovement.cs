d9072322ac1d7cd953ec740ae901479a66359510
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-05-10 16:05:53 +0100

    Added a platform that moves between two visible waypoints

---
+++ Slyggdrasil/Assets/Scripts/ObjectMovement.cs
@@ -0,0 +1,34 @@
+//Script used to move an object between waypoints
+//Assisted by Coding in Flow on YouTube
+//https://www.youtube.com/watch?v=UlEE6wjWuCY
+//Script started: 10/05/2023
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] waypoints; //Store game objects as waypoints in the editor
+    [SerializeField]
+    private float velocity = 200.0f;
+
+    private int currentWaypointIndex = 0;
+
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(waypoints[currentWaypointIndex].transform.position, transform.position) < 0.1f)
+        {
+            currentWaypointIndex++;
+            if(currentWaypointIndex >= waypoints.Length)
+            {
+                currentWaypointIndex = 0;
+            }
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[currentWaypointIndex].transform.position, Time.deltaTime*velocity); //Move every frame towards the target waypoint by the set velocity
+    }
+}
