dd420fde6932dc1ef5238b444cf907b4541a091f
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-05-18 14:28:16 +0100

    Updated all collisions to work as intended

--- Slyggdrasil/Assets/Scripts/NextLevel.cs
+++ Slyggdrasil/Assets/Scripts/NextLevel.cs
@@ -9,15 +9,19 @@ public class NextLevel : MonoBehaviour
     public CharacterController player1Controller; //Reference the character controller script for player 1
     public CharacterController player2Controller; //Reference the character controller script for player 2
 
+    int levelNumber;
+
     // Update is called once per frame
     void FixedUpdate()
     {
         
         if (player1Controller.player1passed == true && player2Controller.player2passed == true)
         {
+
             //Change to the next level
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Load the scene at the next index
-            Debug.Log("Level " + SceneManager.GetActiveScene().buildIndex+1 + " loaded.");
+            levelNumber = SceneManager.GetActiveScene().buildIndex + 2;
+            Debug.Log("Level " + levelNumber + " loaded.");
 
         }
 
