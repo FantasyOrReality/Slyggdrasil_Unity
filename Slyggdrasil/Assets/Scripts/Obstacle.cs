7f3bad4ee0e9ec133f89cbfbc32a7ce99e0b8503
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-05-18 16:52:03 +0100

    Added comments to the obstacle script

--- Slyggdrasil/Assets/Scripts/Obstacle.cs
+++ Slyggdrasil/Assets/Scripts/Obstacle.cs
@@ -1,3 +1,4 @@
+//Script to have obstacles spawn randomly from a spawner
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,35 +7,30 @@ public class Obstacle : MonoBehaviour
 {
 
     [SerializeField]
-    GameObject[] obstacle;
+    GameObject[] obstacle; //The objects we will be spawning
     [SerializeField]
-    float existTime = 0.5f;
+    float existTime = 0.5f; //How long until the object is destroyed
     [SerializeField]
-    float minTras;
+    float minTras; //The minimum x spawn position
     [SerializeField]
-    float maxTras;
+    float maxTras; //The maximum x spawn position
 
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        StartCoroutine(ObstacleSpawn());
+        StartCoroutine(ObstacleSpawn()); //Call the ienumerator
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    //Add a fixed update later so the obstacles are destroyed on player collisions
 
-    }
-
-    IEnumerator ObstacleSpawn()
+    IEnumerator ObstacleSpawn() //assign a function to the ienumerator
     {
         while(true)
         {
-            var WANTED = Random.Range(minTras, maxTras);
-            var POSITION = new Vector3(WANTED, transform.position.y);
-            GameObject gameObject = Instantiate(obstacle[Random.Range(0, obstacle.Length)], POSITION, Quaternion.identity);
-            yield return new WaitForSeconds(existTime);
-            Destroy(gameObject, 2.0f);
+            var WANTED = Random.Range(minTras, maxTras); //Set up the x range that obstacles will spawn between
+            var POSITION = new Vector3(WANTED, transform.position.y); //Set up the spawn location in the world
+            GameObject gameObject = Instantiate(obstacle[Random.Range(0, obstacle.Length)], POSITION, Quaternion.identity); //Spawn the obstacle on the screen
+            yield return new WaitForSeconds(existTime); //Wait a set time for the obstacle's existence
+            Destroy(gameObject, 2.0f); //Destroy the object after a set time. Could use an editable variable to adjust the second value
         }
     }
 
