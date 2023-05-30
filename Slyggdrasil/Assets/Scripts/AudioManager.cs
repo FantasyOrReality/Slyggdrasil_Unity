71449f9152445b7bc2349c61d5f3dd41eccc9eca
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-05-30 14:04:01 +0100

    Added playing background music, still working on the playing function

--- Slyggdrasil/Assets/Scripts/AudioManager.cs
+++ Slyggdrasil/Assets/Scripts/AudioManager.cs
@@ -1,18 +1,106 @@
 //Script to manage audio and music with volume
 using UnityEngine.Audio;
+using System;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class AudioManager : MonoBehaviour
 {
+    public Sound[] sounds;
+    public bool playing;
+
+    public static AudioManager instance;
     // Start is called before the first frame update
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+
+        Play("BackgroundMusic");
+
+
+    }
     void Start()
     {
-        
+        Play("BackgroundMusic");
+
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        foreach (Sound s in sounds)
+        { 
+            if (SceneManager.GetActiveScene().buildIndex < 1 || SceneManager.GetActiveScene().buildIndex > 5)
+            {
+                MuteAudio(true, s);
+            }
+            else
+            {
+                MuteAudio(false, s);
+            }
+        }
+    }
+
+    public void Play(string name)
+    {
         
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        if (playing)
+        {
+            s.source.Play();
+            playing = false;
+        }
+    }
+
+    public bool MuteAudio(bool mute, Sound s)
+    {
+        if (mute)
+        {
+            //s.source = gameObject.AddComponent<AudioSource>();
+            s.volume = 0.0f;
+            s.source.volume = s.volume;
+            s.source.Pause();
+            playing = false;
+            return playing;
+
+
+        }
+        else
+        {
+            //s.source = gameObject.AddComponent<AudioSource>();
+            s.volume = 100.0f;
+            s.source.volume = s.volume;
+            playing = true;
+            return playing;
+
+        }
     }
 }
--- Slyggdrasil/Assets/Scripts/DontDestroy.cs
+++ Slyggdrasil/Assets/Scripts/DontDestroy.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class DontDestroy : MonoBehaviour
 {
     void Start()
     {
-        //Keep this object between scenes
-        GameObject.DontDestroyOnLoad(this.gameObject);
+        if(SceneManager.GetActiveScene().buildIndex >=1 && SceneManager.GetActiveScene().buildIndex <=5)
+        {
+            //Keep this object between scenes
+            GameObject.DontDestroyOnLoad(this.gameObject);
+            //FindObjectOfType<AudioManager>().Play("Slyggdrasil_-_The_Climb");
+        }
+        
     }
 }
