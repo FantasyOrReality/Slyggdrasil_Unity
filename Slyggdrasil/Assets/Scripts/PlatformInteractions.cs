f7a461254ba690866d838ed5dee2ac0e6ea8080b
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-07-03 19:17:08 +0100

    Level 7 revamp finished

--- Slyggdrasil/Assets/Scripts/PlatformInteractions.cs
+++ Slyggdrasil/Assets/Scripts/PlatformInteractions.cs
@@ -20,8 +20,8 @@ public class PlatformInteractions : MonoBehaviour
         //Collided with rigidbody
         Rigidbody2D rb = collision.collider.GetComponent<Rigidbody2D>();
 
-        //collision has a rigidbody?
-        if (rb != null)
+        //collision has a rigidbody and is not an obstacle?
+        if (rb != null && !rb.CompareTag("Obstacle") )
         {
             //Collision from above?
             if (collision.relativeVelocity.y <= 0.0f)
@@ -60,6 +60,47 @@ public class PlatformInteractions : MonoBehaviour
 
             
 
-        }    
+        }
+        //collision has a rigidbody and is an obstacle?
+        else if (rb != null && rb.CompareTag("Obstacle"))
+        {
+            //Collision from above?
+            if (collision.relativeVelocity.y <= 0.0f)
+            {
+                //Base platforms
+                if (this.CompareTag("Base"))
+                {
+                    Vector2 velocityBase = rb.velocity;
+                    velocityBase.y = baseJumpForce/2; //Set the velocity to the pre-determined jumping height
+                    rb.velocity = velocityBase; //Set the collided object's velocity to our velocity variable
+                }
+                //Ice platforms
+                if (this.CompareTag("Ice"))
+                {
+                    Vector2 velocityIce = rb.velocity;
+                    velocityIce.y = iceJumpForce/2; //Set the velocity to the pre-determined jumping height
+                    rb.velocity = velocityIce; //Set the collided object's velocity to our velocity variable
+                }
+                //Fake platforms
+                if (this.CompareTag("Fake"))
+                {
+                    Vector2 velocityFake = rb.velocity;
+                    velocityFake.y = fakeJumpForce/2; //Set the velocity to the pre-determined jumping height
+                    rb.velocity = velocityFake; //Set the collided object's velocity to our velocity variable
+                }
+                //Moving platforms
+                if (this.CompareTag("Moving"))
+                {
+                    Vector2 velocityMoving = rb.velocity;
+                    velocityMoving.y = movingJumpForce/2; //Set the velocity to the pre-determined jumping height
+                    rb.velocity = velocityMoving; //Set the collided object's velocity to our velocity variable
+                }
+            }
+            //Collision from below?
+            //Do nothing
+
+
+
+        }
     }
 }
