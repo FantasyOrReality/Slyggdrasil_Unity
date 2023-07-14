48f17a50b6c1d8850784b4ee76dc6e9dd2fab86c
FantasyOrReality <99820988+FantasyOrReality@users.noreply.github.com>
2023-07-14 16:36:42 +0100

    Added transition between the cutscene and singleplayer level 1

--- Slyggdrasil/Assets/Scripts/ChangeSceneOnTimer.cs
+++ Slyggdrasil/Assets/Scripts/ChangeSceneOnTimer.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class ChangeSceneOnTimer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float changeTime;
+    public string sceneName;
 
     // Update is called once per frame
     void Update()
     {
-        
+        changeTime -= Time.deltaTime;
+        if(changeTime <=0)
+        {
+            SceneManager.LoadScene(sceneName);
+
+        }
     }
 }
